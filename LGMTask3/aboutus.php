164ffee533f460f63cd46baf816d234e5727fe15
aishrangesh <54407459+aishrangesh@users.noreply.github.com>
2021-07-28 00:13:16 +0530

    Add files via upload

---
+++ LGMTask3/aboutus.php
@@ -0,0 +1,28 @@
+<?php include('topbar.php');?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/aboutus.css">
+</head>
+<body>
+
+<div class="about-section">
+  <h1>Aishwarya Rangesh</h1>
+</div>
+
+<h2 style="text-align:center">About me</h2>
+<p style="text-align:center">
+               <img src="img/pic.jpeg" alt="Aishwarya Rangesh" style="width:30%">
+               <center>
+                    <h2>Details</h2>
+                    <p>I'm a third year undergraduate currently interning at LetsGrowMore</p>
+                    <p>Ph No.   :   +91 - 9525987622</p>
+                    <p>aishwaryarangesh6@gmail.com</p>
+                    <p><a href="https://forms.gle/dJRt4DuWtqh417n19"> <button class="button">Contact Me</button></a></p>
+                    </center>
+</div>
+</p>  
+</body>
+</html>
---
+++ LGMTask3/contactus.php
@@ -0,0 +1,21 @@
+<?php include('topbar.php');?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/contactus.css">
+	<title></title>
+</head>
+<body style="background-color: skyblue;">
+    <br><br><br>
+    <div class="box" >
+    	<center><u><h1>Contact</h1></u></center>
+         <ul style="font-size: 30px;">
+         	<li>Location - Bengaluru</li>
+			 <li>State - Karnataka</li>
+            <li>Pin - 560022</li>
+         	<li>Contact no - +91 9525987622</li>
+         </ul>
+         <center><h2>Thank you!</h2></center>
+    </div>
+</body>
+</html>
